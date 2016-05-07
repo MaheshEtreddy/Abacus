47ac7af455e03fb78bb855254ea053822aa7993f
Mahesh Etreddy <mahesh.etreddy@outlook.com>
2016-05-07 17:28:04 +0530

    Revert "Revert "test""
    This reverts commit 2160493fc56c942f10b6c157a217854f2fa40315.

---
+++ AbacusAssignment/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  AbacusAssignment
+//
+//  Created by Mahesh Etreddy on 07/05/16.
+//  Copyright © 2016 Mahesh Etreddy. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var lblName: UILabel!
+}
